48de2569b777363a6040aafa2e7c8dcf3e7321f7
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-26 13:42:24 -0700

    starts Project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+	height: 100vh;
+	background-color: green;
+`;
 
 function App() {
 	return (
 		<>
-			<div className="App">
+			<Container>
 				<h2>React Website</h2>
-			</div>
+			</Container>
 		</>
 	);
 }
