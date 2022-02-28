447dddb27cac3a2c9a1e02a5bf1a3ea0b0738f7b
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-28 09:32:41 -0700

    creates a folder called componenets, creates Navbar component inside it

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import Navbar from "./components/Navbar";
 
 const Container = styled.div`
 	height: 100vh;
-	background-color: green;
 `;
 
 function App() {
 	return (
 		<>
 			<Container>
-				<h2>React Website</h2>
+				<Navbar />
 			</Container>
 		</>
 	);
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+	height: 50px;
+`;
+
+export default function Navbar() {
+	return (
+		<>
+			<Container>Navbar</Container>
+		</>
+	);
+}
