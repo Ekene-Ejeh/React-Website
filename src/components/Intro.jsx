66ea58ac69e7b47306fb352f1effb4092df4bd9a
Ekene Ejeh <ekeneejeh@gmail.com>
2022-03-01 23:43:26 -0700

    Done styling the left side of the Intro component

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,13 +3,88 @@ import styled from "styled-components";
 
 const Container = styled.div`
 	height: calc(100vh - 50px);
-	background-color: red;
+	display: flex;
+	padding: 20px;
+`;
+const Left = styled.div`
+	width: 60%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`;
+
+const Title = styled.h1`
+	font-size: 60px;
+	width: 60%;
+`;
+
+const Desc = styled.p`
+	width: 60%;
+	font-size: 20px;
+	margin-top: 20px;
+`;
+
+const Info = styled.div`
+	width: 60%;
+	display: flex;
+	justify-content: space-between;
+	margin-top: 30px;
+	align-items: center;
+`;
+
+const Button = styled.button`
+	padding: 15px;
+	background-color: darkblue;
+	color: #fff;
+	border-radius: 20px;
+	font-weight: bold;
+	cursor: pointer;
+	border: none;
+	letter-spacing: 2px;
+`;
+
+const Contact = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+const Phone = styled.span`
+	color: tomato;
+	font-weight: bold;
+`;
+const ContactText = styled.span`
+	color: grey;
+	margin-top: 5px;
+`;
+
+const Right = styled.div`
+	width: 40%;
 `;
 
 export default function Intro() {
 	return (
 		<>
-			<Container></Container>
+			<Container>
+				<Left>
+					<Title>Adventures in Creative age</Title>
+					<Desc>
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi,
+						magni cum magnam hic quo aspernatur perferendis temporibus modi, ex
+						nostrum tempora, nesciunt iste a quam iusto assumenda. Expedita nisi
+						inventore numquam molestias optio aperiam itaque, facere quasi id in
+						ab unde cum minima.
+					</Desc>
+					<Info>
+						<Button>START A PROJECT</Button>
+						<Contact>
+							<Phone>Call Us +234 8061234567</Phone>
+							<ContactText>For any questions and concerns</ContactText>
+						</Contact>
+					</Info>
+				</Left>
+				<Right>R</Right>
+			</Container>
 		</>
 	);
 }
