d7d0a620c68c7fe6ef4f8c22d7d4a67c19c419bf
Ekene Ejeh <ekeneejeh@gmail.com>
2022-03-03 15:29:48 -0700

    created a component called Service

---
+++ src/components/Service.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+	display: flex;
+`;
+
+const Left = styled.div`
+	width: 50%;
+`;
+
+const Right = styled.div`
+	width: 50%;
+`;
+
+export default function Service() {
+	return (
+		<>
+			<div>Service</div>
+		</>
+	);
+}
