6ed8d0fe882b017a0cf6c97f01e978aaa7f83fee
Ekene Ejeh <ekeneejeh@gmail.com>
2022-03-04 13:58:04 -0700

    creates Price Component

---
+++ src/components/Price.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+	height: 100%;
+	display: flex;
+`;
+
+export default function Price() {
+	return (
+		<>
+			<Container>price</Container>
+		</>
+	);
+}
