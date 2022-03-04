3beaa84b6c864f6bf98f1722d406c4db459ac55a
Ekene Ejeh <ekeneejeh@gmail.com>
2022-03-04 12:40:53 -0700

    styles the MiniCard Component

--- src/components/MiniCard.jsx
+++ src/components/MiniCard.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 
+const Container = styled.div``;
+const Image = styled.img``;
+const Text = styled.p``;
+
 export default function MiniCard() {
 	return (
 		<>
-			<div>MiniCard</div>
+			<Container>
+				<Image></Image>
+				<Text>
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere,
+					enim.
+				</Text>
+			</Container>
 		</>
 	);
 }
