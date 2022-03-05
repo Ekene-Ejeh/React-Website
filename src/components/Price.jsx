93371b657acb024327d01cdd969fdd22fa39bfe6
Ekene Ejeh <ekeneejeh@gmail.com>
2022-03-05 09:03:45 -0700

    created PriceCard Component

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import PriceCard from "./PriceCard";
 
 const Container = styled.div`
 	height: 100%;
 	display: flex;
+	justify-content: center;
 `;
 
 export default function Price() {
 	return (
 		<>
-			<Container>price</Container>
+			<Container>
+				<PriceCard />
+				<PriceCard />
+				<PriceCard />
+			</Container>
 		</>
 	);
 }
