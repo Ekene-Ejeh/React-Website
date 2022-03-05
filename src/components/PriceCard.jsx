dcbe582bdbbe7e1ba89e2d170d7d2a8be1a1a221
Ekene Ejeh <ekeneejeh@gmail.com>
2022-03-05 09:18:28 -0700

    starts styling the priceCard Component

--- src/components/PriceCard.jsx
+++ src/components/PriceCard.jsx
@@ -2,11 +2,29 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div``;
+const PriceContainer = styled.div``;
+const Price = styled.div``;
+const Type = styled.div``;
+const List = styled.div``;
+const ListItem = styled.div``;
+const Button = styled.div``;
 
 export default function PriceCard() {
 	return (
 		<>
-			<Container>PriceCard</Container>
+			<Container>
+				<PriceContainer>
+					$<Price>20</Price>/month
+					<Type>Basic Plan</Type>
+					<List>
+						<ListItem>200 Hand-Crafted templates</ListItem>
+						<ListItem>Exclusive Support</ListItem>
+						<ListItem>50+ Prebuilt Websites</ListItem>
+						<ListItem>Premium Plugins</ListItem>{" "}
+					</List>
+					<Button>Join Now</Button>
+				</PriceContainer>
+			</Container>
 		</>
 	);
 }
