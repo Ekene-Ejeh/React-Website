02b200fadc1ea028e9de8ddd507008cb41fe035d
Ekene Ejeh <ekeneejeh@gmail.com>
2022-03-06 09:45:52 -0700

    starts styling the Footer Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+	height: 10%;
+	background-color: #111;
+	color: lightgray;
+`;
+const Wrapper = styled.div``;
+const List = styled.ul``;
+const ListItem = styled.li``;
+const Copyright = styled.span``;
 
 export default function Footer() {
 	return (
 		<>
-			<Container></Container>
+			<Container>
+				<Wrapper>
+					<List>
+						<ListItem>Guide</ListItem>
+						<ListItem>Support</ListItem>
+						<ListItem>API</ListItem>
+						<ListItem>Community</ListItem>
+					</List>
+					<Copyright>Ekene Dev </Copyright>
+				</Wrapper>
+			</Container>
 		</>
 	);
 }
