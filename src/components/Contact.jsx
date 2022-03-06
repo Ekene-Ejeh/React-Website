bef58adc59557df7c534fff5264ebc48f00da499
Ekene Ejeh <ekeneejeh@gmail.com>
2022-03-06 09:26:18 -0700

    Done styling the Contact Component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import Map from "../img/map.png";
+import Phone from "../img/phone.png";
+import Send from "../img/send.png";
 
 const Container = styled.div`
 	height: 90%;
@@ -16,8 +19,77 @@ const Wrapper = styled.div`
 const FormContainer = styled.div`
 	width: 50%;
 `;
+
+const Title = styled.h1`
+	margin: 50px;
+	margin-top: 0;
+	margin-left: 70px;
+`;
+
+const Form = styled.form`
+	height: 250px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+const LeftForm = styled.div`
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	margin-right: 20px;
+`;
+
+const RightForm = styled.div`
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+`;
+
+const Input = styled.input`
+	width: 200px;
+	padding: 20px;
+`;
+
+const TextArea = styled.textarea`
+	width: 200px;
+	height: 60%;
+	padding: 20px;
+`;
+
+const Button = styled.button`
+	border: none;
+	padding: 15px;
+	background-color: darkblue;
+	color: #fff;
+	font-size: 20px;
+	border-radius: 15px;
+	margin-top: 20px;
+	cursor: pointer;
+`;
+
 const AddressContainer = styled.div`
 	width: 50%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+const AddressItem = styled.div`
+	display: flex;
+	align-items: center;
+	margin-bottom: 50px;
+`;
+const Icon = styled.img`
+	width: 20px;
+	margin-right: 20px;
+`;
+
+const Text = styled.span`
+	font-size: 20px;
+	margin-right: 20px;
 `;
 
 export default function Contact() {
@@ -25,8 +97,38 @@ export default function Contact() {
 		<>
 			<Container>
 				<Wrapper>
-					<FormContainer></FormContainer>
-					<AddressContainer></AddressContainer>
+					<FormContainer>
+						<Title>
+							Questions ? <br /> Lets get in touch
+						</Title>
+						<Form>
+							<LeftForm>
+								<Input placeholder="Your Name" />
+								<Input placeholder="Your Email" />
+								<Input placeholder="Subject" />
+							</LeftForm>
+							<RightForm>
+								<TextArea placeholder="Your Message" />
+								<Button>Send</Button>
+							</RightForm>
+						</Form>
+					</FormContainer>
+					<AddressContainer>
+						<AddressItem>
+							<Icon src={Map} />
+							<Text>123 Park Avenue st., New York, USA</Text>
+						</AddressItem>
+						<AddressItem>
+							<Icon src={Phone} />
+							<Text>+234 8034567890</Text>
+							<Text>+234 8031234567</Text>
+						</AddressItem>
+						<AddressItem>
+							<Icon src={Send} />
+							<Text>contact@example.com</Text>
+							<Text>info@example.com</Text>
+						</AddressItem>
+					</AddressContainer>
 				</Wrapper>
 			</Container>
 		</>
